47bddcdab05eee7073f9b7bb5f1d7515ab26176a
qingbo1011 <qingbo1011@163.com>
2023-04-02 20:53:34 +0800

    引入中间件

--- example/main.go
+++ example/main.go
@@ -5,7 +5,8 @@ import (
 	"github.com/qingbo1011/qiaomu"
 )
 
-func main() {
+// 路由测试
+/*func main() {
 	engine := qiaomu.New()
 	group := engine.Group("user")
 	group.Get("/hello", func(ctx *qiaomu.Context) {
@@ -27,5 +28,43 @@ func main() {
 	group.Post("/log/*", func(ctx *qiaomu.Context) {
 		fmt.Fprint(ctx.W, "Post,/log/*")
 	})
+	engine.Run()
+}*/
+
+// Log 定义一个测试中间件
+func Log(next qiaomu.HandlerFunc) qiaomu.HandlerFunc {
+	return func(ctx *qiaomu.Context) {
+		fmt.Println("打印请求参数")
+		next(ctx)
+		fmt.Println("返回执行时间")
+	}
+}
+
+// 中间件测试
+func main() {
+	engine := qiaomu.New()
+	group := engine.Group("user")
+	// 具体路由使用中间件
+	group.Get("/hello/get", func(ctx *qiaomu.Context) {
+		fmt.Println("handler")
+		fmt.Fprint(ctx.W, "/hello/get GET")
+	}, Log)
+	group.Get("/hello/get2", func(ctx *qiaomu.Context) {
+		fmt.Println("handler")
+		fmt.Fprint(ctx.W, "/hello/get2 GET")
+	})
+
+	// 整个路由组使用中间件
+	group2 := engine.Group("student")
+	group2.Use(Log)
+	group2.Get("/info", func(ctx *qiaomu.Context) {
+		fmt.Println("handler")
+		fmt.Fprint(ctx.W, "/info GET")
+	})
+	group2.Get("/info2", func(ctx *qiaomu.Context) {
+		fmt.Println("handler")
+		fmt.Fprint(ctx.W, "/info2 GET")
+	})
+
 	engine.Run()
 }
--- qiaomu/queen.go
+++ qiaomu/queen.go
@@ -13,6 +13,8 @@ const (
 
 type HandlerFunc func(ctx *Context)
 
+type MiddlewareFunc func(handlerFunc HandlerFunc) HandlerFunc
+
 type Context struct {
 	W http.ResponseWriter
 	R *http.Request
@@ -31,16 +33,19 @@ func (r *router) Group(name string) *routerGroup {
 			name:     "/",
 			children: make([]*treeNode, 0),
 		},
+		middlewaresFuncMap: make(map[string]map[string][]MiddlewareFunc),
 	}
 	r.groups = append(r.groups, g)
 	return g
 }
 
 type routerGroup struct {
-	groupName        string
-	handlerMap       map[string]map[string]HandlerFunc
-	handlerMethodMap map[string][]string
-	treeNode         *treeNode
+	groupName          string
+	handlerMap         map[string]map[string]HandlerFunc
+	handlerMethodMap   map[string][]string
+	treeNode           *treeNode
+	middlewaresFuncMap map[string]map[string][]MiddlewareFunc
+	middlewares        []MiddlewareFunc
 }
 
 // Handle method的有效性校验
@@ -49,66 +54,91 @@ func (r *routerGroup) Handle(name string, method string, handlerFunc HandlerFunc
 }
 
 // Any 任意类型的路由
-func (r *routerGroup) Any(name string, handleFunc HandlerFunc) {
-	r.handle(name, MethodAny, handleFunc)
+func (r *routerGroup) Any(name string, handleFunc HandlerFunc, middlewareFunc ...MiddlewareFunc) {
+	r.handle(name, MethodAny, handleFunc, middlewareFunc...)
 }
 
 // Get Get类型路由
-func (r *routerGroup) Get(name string, handleFunc HandlerFunc) {
-	r.handle(name, http.MethodGet, handleFunc)
+func (r *routerGroup) Get(name string, handleFunc HandlerFunc, middlewareFunc ...MiddlewareFunc) {
+	r.handle(name, http.MethodGet, handleFunc, middlewareFunc...)
 }
 
 // Head Head类型路由
-func (r *routerGroup) Head(name string, handleFunc HandlerFunc) {
-	r.handle(name, http.MethodHead, handleFunc)
+func (r *routerGroup) Head(name string, handleFunc HandlerFunc, middlewareFunc ...MiddlewareFunc) {
+	r.handle(name, http.MethodHead, handleFunc, middlewareFunc...)
 }
 
 // Post Post类型路由
-func (r *routerGroup) Post(name string, handleFunc HandlerFunc) {
-	r.handle(name, http.MethodPost, handleFunc)
+func (r *routerGroup) Post(name string, handleFunc HandlerFunc, middlewareFunc ...MiddlewareFunc) {
+	r.handle(name, http.MethodPost, handleFunc, middlewareFunc...)
 }
 
 // Put Put类型路由
-func (r *routerGroup) Put(name string, handleFunc HandlerFunc) {
-	r.handle(name, http.MethodPut, handleFunc)
+func (r *routerGroup) Put(name string, handleFunc HandlerFunc, middlewareFunc ...MiddlewareFunc) {
+	r.handle(name, http.MethodPut, handleFunc, middlewareFunc...)
 }
 
 // Patch Patch类型路由
-func (r *routerGroup) Patch(name string, handleFunc HandlerFunc) {
-	r.handle(name, http.MethodPatch, handleFunc)
+func (r *routerGroup) Patch(name string, handleFunc HandlerFunc, middlewareFunc ...MiddlewareFunc) {
+	r.handle(name, http.MethodPatch, handleFunc, middlewareFunc...)
 }
 
 // Delete Delete类型路由
-func (r *routerGroup) Delete(name string, handleFunc HandlerFunc) {
-	r.handle(name, http.MethodDelete, handleFunc)
+func (r *routerGroup) Delete(name string, handleFunc HandlerFunc, middlewareFunc ...MiddlewareFunc) {
+	r.handle(name, http.MethodDelete, handleFunc, middlewareFunc...)
 }
 
 // Connect Connect类型路由
-func (r *routerGroup) Connect(name string, handleFunc HandlerFunc) {
-	r.handle(name, http.MethodConnect, handleFunc)
+func (r *routerGroup) Connect(name string, handleFunc HandlerFunc, middlewareFunc ...MiddlewareFunc) {
+	r.handle(name, http.MethodConnect, handleFunc, middlewareFunc...)
 }
 
 // Options Options类型路由
-func (r *routerGroup) Options(name string, handleFunc HandlerFunc) {
-	r.handle(name, http.MethodOptions, handleFunc)
+func (r *routerGroup) Options(name string, handleFunc HandlerFunc, middlewareFunc ...MiddlewareFunc) {
+	r.handle(name, http.MethodOptions, handleFunc, middlewareFunc...)
 }
 
 // Trace Trace类型路由
-func (r *routerGroup) Trace(name string, handleFunc HandlerFunc) {
-	r.handle(name, http.MethodTrace, handleFunc)
+func (r *routerGroup) Trace(name string, handleFunc HandlerFunc, middlewareFunc ...MiddlewareFunc) {
+	r.handle(name, http.MethodTrace, handleFunc, middlewareFunc...)
 }
 
 // 统一处理
-func (r *routerGroup) handle(name string, method string, handlerFunc HandlerFunc) {
+func (r *routerGroup) handle(name string, method string, handlerFunc HandlerFunc, middlewareFunc ...MiddlewareFunc) {
 	_, ok := r.handlerMap[name]
 	if !ok {
 		r.handlerMap[name] = make(map[string]HandlerFunc)
+		r.middlewaresFuncMap[name] = make(map[string][]MiddlewareFunc)
 	}
 	r.handlerMap[name][method] = handlerFunc
 	r.handlerMethodMap[method] = append(r.handlerMethodMap[method], name)
 	methodMap := make(map[string]HandlerFunc)
 	methodMap[method] = handlerFunc
+	r.middlewaresFuncMap[name][method] = append(r.middlewaresFuncMap[name][method], middlewareFunc...) // 添加中间件
 	r.treeNode.Put(name)
+}
+
+func (r *routerGroup) methodHandle(ctx *Context, name string, method string, handler HandlerFunc) {
+	//组通用中间件
+	if r.middlewares != nil {
+		for _, middlewareFunc := range r.middlewares {
+			handler = middlewareFunc(handler)
+		}
+	}
+	//组路由级别
+	middlewareFuncs := r.middlewaresFuncMap[name][method]
+	if middlewareFuncs != nil {
+		for _, middlewareFunc := range middlewareFuncs {
+			handler = middlewareFunc(handler)
+		}
+	}
+	// 执行路由逻辑
+	handler(ctx)
+}
+
+// Use 注册中间件
+func (r *routerGroup) Use(middlewareFunc ...MiddlewareFunc) {
+	r.middlewares = append(r.middlewares, middlewareFunc...)
 }
 
 type Engine struct {
@@ -122,6 +152,10 @@ func New() *Engine {
 }
 
 func (e *Engine) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	e.httpRequestHandle(w, r)
+}
+
+func (e *Engine) httpRequestHandle(w http.ResponseWriter, r *http.Request) {
 	method := r.Method
 	for _, group := range e.router.groups {
 		routerName := utils.SubStringLast(r.RequestURI, utils.ConcatenatedString([]string{"/", group.groupName}))
@@ -134,24 +168,24 @@ func (e *Engine) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 			// ANY下的匹配
 			handler, ok := group.handlerMap[node.routerName][MethodAny]
 			if ok {
-				handler(ctx)
+				group.methodHandle(ctx, node.routerName, MethodAny, handler)
 				return
 			}
 			// 指定Method的匹配（如Get，Post）
 			handler, ok = group.handlerMap[node.routerName][method]
 			if ok {
-				handler(ctx)
+				group.methodHandle(ctx, node.routerName, method, handler)
 				return
 			}
 			// url匹配但请求方式不匹配，405 MethodNotAllowed
 			w.WriteHeader(http.StatusMethodNotAllowed)
 			fmt.Fprintln(w, utils.ConcatenatedString([]string{method, " not allowed"}))
 			return
-		} else { // 路由匹配失败，404 NotFound
-			w.WriteHeader(http.StatusNotFound)
-			fmt.Fprintln(w, r.RequestURI+" not found")
 		}
 	}
+	// 路由匹配失败，404 NotFound
+	w.WriteHeader(http.StatusNotFound)
+	fmt.Fprintln(w, r.RequestURI+" not found")
 }
 
 func (e *Engine) Run() {
